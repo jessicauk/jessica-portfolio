68c57af967cae3d0424acdf5e84d2b53237f2bee
japolinar <jessica.apolinar@globalcorporation.cc>
2016-04-22 13:13:23 -0500

    rutas

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,27 @@ angular.module('portfolioApp',['ui.router'])
 	.run(function ($rootScope) {
 		$rootScope.mensaje = "Angular tools";
 	})
-	.config(function ( $locationProvider, $stateProvider, $urlRouterProvider ) {
+	.config( function ( $locationProvider, $stateProvider, $urlRouterProvider ) {
 		$locationProvider.html5Mode({
 			enabled: true,
   			requireBase: false
 		});
-		$urlRouterProvider.otherwise("/index")
+		//$urlRouterProvider.otherwise("/index")
 		$stateProvider
-			.state('about', {
-				url : '/about',
-				templateUrl : '../templates/about/aboutme.html',
-				controller : 'mainController'
+			.state('index', {
+				url : '',
+				views : {
+					"header" : {templateUrl : "../templates/header.html"}
+				}
 			})
+				.state('about', {
+					url: '/about',
+					templateUrl: '../templates/about.html',
+					controller: function ($scope) {
+						$scope.works = ['Everis México', 'Morton Casa de Subastas', 'Global Human Services'];
+					}
+				})
+
 	})
 	.controller('mainController', ['$scope', '$rootScope', '$http', function ( $scope, $rootScope, $http ) {
 		$scope.message = $rootScope.mensaje;
