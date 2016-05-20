af8ffcb4517f339bad83cdf8c5cbb7b1536f66ab
japolinar <jessica.apolinar@globalcorporation.cc>
2016-05-20 12:04:09 -0500

    api music styles

--- assets/js/controllers/work/music.js
+++ assets/js/controllers/work/music.js
@@ -8,13 +8,28 @@ angular.module('music.module', [])
 			servicesFactory.searchItem($scope.baseUrl, type, artist)
 				.success( function (res) {
 					$scope.loader = false;
-					console.log(res.artists.items[0].name);
+					$scope.emptyArray = false;
+					$scope.artista ="";
+					//console.log(res.artists.items[0].name);
 					$scope.artista = res.artists.items[0];
-					$scope.img = $scope.artista.images[0].url;
-					$scope.genres = $scope.artista.genres;
-					$scope.idArtist = $scope.artista.id;
-					$scope.artistName = $scope.artista.name;
-					$scope.artistAlbum($scope.idArtist);
+					if($scope.artista !== undefined) {
+						$scope.artist="";
+						$scope.emptyArray = false;
+						$scope.img = $scope.artista.images[0].url;
+						$scope.genres = $scope.artista.genres;
+						$scope.idArtist = $scope.artista.id;
+						$scope.artistName = $scope.artista.name;
+						$scope.artistAlbum($scope.idArtist);
+					}else{
+						$scope.artista = [];
+						$scope.img ="";
+						$scope.genres = [];
+						$scope.artistName ="";
+						$scope.emptyArray = true;
+						//$scope.artist="";
+						$scope.message="Artist not found, try once!";
+					}
+
 				})
 				.error(function (err) {
 					console.log("error D:");
@@ -24,7 +39,8 @@ angular.module('music.module', [])
 		$scope.artistAlbum = function (idArtist) {
 			servicesFactory.albumArtist($scope.baseUrl, idArtist)
 				.success( function (res) {
-					console.log("album ok : " + JSON.stringify(res));
+					//console.log("album ok : " + JSON.stringify(res));
+					$scope.albums = res.items;
 				})
 				.error(function (err) {
 					console.log("album no : ");
